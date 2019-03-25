907023f0fbd9606bc7278cfad141ec7af77cc2e7
Archinect <Da.kapapreta@yandex.ru>
2019-03-26 00:55:54 +0300

    Update game.js

--- game.js
+++ game.js
@@ -59,7 +59,7 @@ function draw() {
  for(var i = 0; i < pipe.length; i++) {
  ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
  ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
- ctx.drawImage(cakepng, pipe[i].x, pipe[i].y-30);
+ ctx.drawImage(cakepng, pipe[i].x, pipe[i].y+pipeUp.height+30);
 
  pipe[i].x--;
 
